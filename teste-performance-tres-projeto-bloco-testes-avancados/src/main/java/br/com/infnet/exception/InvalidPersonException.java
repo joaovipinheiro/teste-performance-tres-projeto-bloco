cab7b5d9d41d7b3815ef13855dc8119d5834966b
João Victor Pinheiro <127742336+joaovipinheiro@users.noreply.github.com>
2025-11-15 19:29:45 -0300

    Delete teste-performance-tres-projeto-bloco-testes-avancados directory

--- teste-performance-tres-projeto-bloco-testes-avancados/src/main/java/br/com/infnet/exception/InvalidPersonException.java
+++
@@ -1,7 +0,0 @@
-package br.com.infnet.exception;
-
-public class InvalidPersonException extends RuntimeException {
-    public InvalidPersonException(String message) {
-        super(message);
-    }
-}
--- teste-performance-tres-projeto-bloco-testes-avancados/src/main/java/br/com/infnet/exception/PersonNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.infnet.exception;
-
-public class PersonNotFoundException extends RuntimeException {
-    public PersonNotFoundException(String id) {
-        super("Usuário com ID " + id + " não encontrado.");
-    }
-}
